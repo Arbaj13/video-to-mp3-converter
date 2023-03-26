13c8267d9eaee2b545ca5a378581d9acc0a96fa6
Arbaj Pathan <arbaj22p@gmail.com>
2023-03-26 16:47:37 +0530

    notification service added

--- src/notification/send/email.py
+++ src/notification/send/email.py
@@ -5,8 +5,8 @@ def notification(message):
     try:
         message = json.loads(message)
         mp3_fid = message["mp3_fid"]
-        sender_address = os.environ.get("GEMAIL_ADDRESS")
-        sender_password = os.environ.get("GEMAIL_PASSWORD")
+        sender_address = os.environ.get("GMAIL_ADDRESS")
+        sender_password = os.environ.get("GMAIL_PASSWORD")
         receiver_address = message['usernme']
 
         msg = EmailMessage()
